b8773440d513594507fb38866a774cbd3eb685fd
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2024-05-07 21:00:53 +0330

    render the list of products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,12 +1,22 @@
-import { useProducts } from "../context/ProductsContext"
+import { useProducts } from "../context/ProductsContext";
+
+import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
-const products=useProducts()
-console.log(products)
+  const products = useProducts();
+  console.log(products);
 
   return (
-    <div>ProductsPage</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>loading...</p>}
+        {products.map((product) => (
+          <p key={product.id}>{product.title}</p>
+        ))}
+      </div>
+      <div>sidebar</div>
+    </div>
+  );
 }
 
-export default ProductsPage
+export default ProductsPage;
