77efe13c466cd506ab485681384589be51d65478
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2024-05-08 23:47:44 +0330

    Create a search field

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductsContext";
@@ -6,18 +9,35 @@ import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
+  const [search, setSearch] = useState("");
   console.log(products);
 
+  const searchHandler = () => {
+    console.log("search");
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>sidebar</div>
+      </div>
+    </>
   );
 }
 
