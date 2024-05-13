522a7d0ba3184e5267e4d1edf7a266e77b818812
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2024-05-13 16:34:50 +0330

    Development of reduce function

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -10,7 +10,6 @@ const initialState = {
 };
 
 const reducer = (state, action) => {
-  console.log(action);
   switch (action.type) {
     case "ADD_ITEM":
       if (!state.selectedItems.find((item) => item.id === action.payload.id)) {
@@ -22,6 +21,47 @@ const reducer = (state, action) => {
         checkout: false,
       };
 
+    case "REMOVE_ITEM":
+      const newSelectedItems = state.selectedItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      return {
+        ...state,
+        selectedItems: [...newSelectedItems],
+        ...sumProducts(newSelectedItems),
+      };
+
+    case "INCREASE":
+      const increaseIndex = state.selectedItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      state.selectedItems[increaseIndex].quantity++;
+
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems),
+      };
+
+    case "DECREASE":
+      const decreaseIndex = state.selectedItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      state.selectedItems[decreaseIndex].quantity--;
+
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems),
+      };
+
+    case "CHECKOUT":
+      return {
+        selectedItems: [],
+        itemsCounter: 0,
+        total: 0,
+        checkout: true,
+      };
     default:
       throw new Error("Invalid Action!");
   }
