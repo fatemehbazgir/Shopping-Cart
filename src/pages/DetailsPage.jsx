c0406baa37bdf0275a74ddaebb54c31ea9273ade
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2024-05-14 23:08:29 +0330

    Product details page

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Link, useParams } from "react-router-dom";
+import { SiOpenproject } from "react-icons/si";
+import { IoMdPricetag } from "react-icons/io";
+import { FaArrowLeft } from "react-icons/fa";
+
+import { useProductDetails } from "../context/ProductsContext";
+import Loader from "../components/Loader";
+
+import styles from "./DetailsPage.module.css";
 
 function DetailsPage() {
+  const { id } = useParams();
+  const productDetails = useProductDetails(+id);
+
+  if (!productDetails) return <Loader />;
   return (
-    <div>DetailsPage</div>
-  )
+    <div className={styles.container}>
+      <img src={productDetails.image} alt={productDetails.title} />
+      <div className={styles.information}>
+        <h3 className={styles.title}>{productDetails.title}</h3>
+        <p className={styles.description}>{productDetails.description}</p>
+        <p className={styles.category}>
+          <SiOpenproject />
+          {productDetails.category}
+        </p>
+        <div>
+          <span className={styles.price}>
+            <IoMdPricetag />
+            {productDetails.price} $
+          </span>
+          <Link to="/products">
+            <FaArrowLeft />
+            <span>Back To Shop</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DetailsPage
+export default DetailsPage;
